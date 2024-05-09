5a59da54bd5c47311a29ad4dc6a5ca73d8d09f47
ahmadabd <ahmadabd13741112@gmail.com>
2024-05-09 12:07:30 +0330

    create dockerfile and first migration

---
+++ delivery-delay-service/database/migrations/2024_05_09_083039_create_vendors_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('vendors', function (Blueprint $table) {
+            $table->increments('id');
+            $table->json('title')->nullable();
+            $table->json('description')->nullable();
+            $table->boolean('active')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('vendors', function (Blueprint $table) {
+            Schema::drop('vendors');
+        });
+    }
+};
